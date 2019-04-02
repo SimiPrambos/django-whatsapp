e3a8b6fc2bb60e843d1bea98108e3d66bd56696f
simi <simi.prambos@gmail.com>
2019-04-02 13:46:32 +0700

    fix serve large file nginx

--- backend/django_whatsapp/urls.py
+++ backend/django_whatsapp/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/', include('api_v1.urls'), name='api'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
